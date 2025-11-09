3e4d853fa53b35f96b2098230fc50e40f5724daf
iyan subdiana <subdiana.project@gmail.com>
2025-11-09 12:01:08 +0700

    implement get all user

---
+++ source/feature/public/get_all_user/handler_impl.go
@@ -0,0 +1,21 @@
+package getalluser
+
+import (
+	"github.com/gin-gonic/gin"
+	httpresputils "github.com/i-sub135/go-rest-blueprint/source/common/glob_utils/http_resp_utils"
+	"github.com/i-sub135/go-rest-blueprint/source/pkg/logger"
+)
+
+func (h *Handler) Impl(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	users, err := h.repo.GetAll(ctx)
+	if err != nil {
+		errMsg := err.Error()
+		logger.Error().Err(err).Caller().Msg(errMsg)
+		httpresputils.HttpRespBadRequest(c, &errMsg)
+		return
+	}
+
+	httpresputils.HttpRespOK(c, users, nil)
+}
---
+++ source/feature/public/get_all_user/repository.go
@@ -0,0 +1,29 @@
+package getalluser
+
+import (
+	"context"
+
+	usermodel "github.com/i-sub135/go-rest-blueprint/source/common/model/user_model"
+	userrepo "github.com/i-sub135/go-rest-blueprint/source/common/repository/user_repo"
+)
+
+type UserRepositories interface {
+	// common repo implement
+	GetAll(ctx context.Context) (*[]usermodel.User, error)
+	/**
+	append methods name for implement internal methode
+		example GetUserByEmail(ctx context.Context) (usermodel.User, error)
+		and implement method/function in repository_impl.go
+
+	*/
+}
+
+type repositoryImpl struct {
+	*userrepo.UserRepo // Embedded shared repo
+}
+
+func injectRepository(userRepo *userrepo.UserRepo) UserRepositories {
+	return &repositoryImpl{
+		UserRepo: userRepo,
+	}
+}
